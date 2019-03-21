30aa3330aa809056c09b065e4130cd75c3d4611d
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-03-21 16:44:59 +0500

    semi-model-done

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class product(models.Model):
+class Product(models.Model):
     firstName = models.CharField(max_length=200)
     lastName = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('products', views.product, name='product')
+    path('products', views.product, name='Product')
 ]
