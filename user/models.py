d4bcba4c620f8ba383033eb45beb86a626de3077
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-03-21 23:14:21 +0500

    problem-fix

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -57,7 +58,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
