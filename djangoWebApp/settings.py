f5989ac23b70b3470aca5305cad74644aa813ada
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-03-21 20:41:30 +0500

    yelo

--- djangoWebApp/settings.py
+++ djangoWebApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'product',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+AUTH_USER_MODEL = 'user.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- user/models.py
+++ user/models.py
@@ -1,3 +1,89 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, firstName="", lastName="", address, password=None, active=True, staff=False, admin=False):
+        if not email:
+            raise ValueError("Users must have a valid Email address")
+        if not staff and not admin:
+            if not firstName:
+                raise ValueError("Users must have a valid First Name")
+            if not lastName:
+                raise ValueError("Users must have a valid Last Name")
+            if not address:
+                raise ValueError("Users must have a valid address")
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.firstName = firstName,
+        user.lastName = lastName,
+        user.address = address,
+        user.staff = staff,
+        user.active = active
+        user.admin = admin
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password=None, firstName="", lastName="", *args, **kwargs):
+        if not password:
+            raise ValueError("User nust have a valid password.")
+        user = self.create_user(
+            email, password,
+            active=True,
+            staff=True,
+            admin=False,
+            firstName=firstName,
+            lastName=firstName
+        )
+        return user
+
+    def create_superuser(self, email, password=None, firstName="", lastName="", *args, **kwargs):
+        if not password:
+            raise ValueError("User nust have a valid password.")
+        user = self.create_user(
+            email, password,
+            active=True,
+            staff=True,
+            admin=True,
+            firstName=firstName,
+            lastName=lastName
+        )
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.firstName + self.lastName
+
+    def get_short_name(self):
+        return self.firstName
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
