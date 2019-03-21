306cb0a231a27d5d2a5ddc848739fd4439dfedfb
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-03-21 19:33:55 +0500

    implemented-product

--- product/models.py
+++ product/models.py
@@ -3,19 +3,34 @@ from django.db import models
 # Create your models here.
 
 
+class singleImage(models.Model):
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name_plural = "zimageContainer"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
 class Product(models.Model):
-    firstName = models.CharField(max_length=200)
-    lastName = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     rating = models.FloatField()
     price = models.FloatField()
-    stock =models.IntegerField()
-    profileImage =models.ImageField()
-    Images =models
-    # Description =
-    # Discount =
-    # Reviews =
-    # body = models.TextField()
+    stock = models.IntegerField()
+    profileImage = models.ImageField()
+    Images = models.ForeignKey(singleImage, on_delete=models.CASCADE)
+    Description = models.TextField()
+    Discount = models.IntegerField()
 
-class Business(models.Model):
-    singleImage=models.ImageField()
+    def __str__(self):
+        return self.name
