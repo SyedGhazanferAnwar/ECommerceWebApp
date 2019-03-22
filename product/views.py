c4b39c718d89cc074edfaf8f3303dec52ad7d569
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-03-22 23:18:27 +0500

    resolved-conflicts

--- product/views.py
+++ product/views.py
@@ -5,23 +5,11 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-<<<<<<< HEAD
-    return render(request,'index.html',{})
-=======
-<<<<<<< HEAD
-    qs = Product.object.all()
-    return H
-=======
     qs = Product.objects.all()
-<<<<<<< HEAD
     for i in qs:
         print(i.profileImage)
     return render(request, "index.html", {"qs":qs})
-=======
-    return HttpResponse(qs)
->>>>>>> b09d5d6921f5b148e68fc85e2a72c4017154df76
->>>>>>> 5883dcb67305cbbf55fcd45fa7eaac372917ce41
->>>>>>> 868ddc6de3cd87ed6d406790e50b0535667643d0
+
 
 
 def product(request):
