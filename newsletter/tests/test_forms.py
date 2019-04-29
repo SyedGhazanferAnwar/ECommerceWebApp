3b3a670af502b8f0ca75aa598d33caa4fffe43d9
alihussam <alihussam.cs@gmail.com>
2019-04-29 18:02:05 +0500

    Add test for contact app

--- newsletter/tests/test_forms.py
+++ newsletter/tests/test_forms.py
@@ -2,20 +2,25 @@ from django.test import TestCase
 from django.forms import ValidationError
 from newsletter.forms import NewsUserForm
 
-class FormTest(TestCase):
-    def test_true_validity(self):
+class TestNewsletterForm(TestCase):
+    def test_newsletter_true_email_validity(self):
         data = {'email':'email@gmail.com'}
         form = NewsUserForm(data=data)
         self.assertTrue(form.is_valid)
     #check if form accepts invalid emails
-    def test_false_validity(self):
-        data = {'email':'emailgmail'}
+    def test_newsletter_false_email_validity(self):
+        data = {'email':'emailgmail@'}
         form = NewsUserForm(data=data)
         if not form.has_error :
             raise ValidationError('NEWSLETTER FORM ACCEPTED INVALID EMAIL')
     #check if form accepts null emails        
-    def test_accept_null_email(self):
+    def test_newsletter_accept_null_email(self):
         data = {'email':None}
         form = NewsUserForm(data=data)
         if not form.has_error:
             raise ValidationError('NEWSLETTER FORM ACCEPTED NULL EMAIL VALUE')
+    def test_newsletter_accept_blank_email(self):
+        data = {'email':''}
+        form = NewsUserForm(data=data)
+        if not form.has_error:
+            raise ValidationError('NEWSLETTER FORM ACCEPTED BLANK EMAIL VALUE')
--- newsletter/tests/test_model.py
+++ newsletter/tests/test_model.py
@@ -2,22 +2,26 @@ from django.shortcuts import get_object_or_404
 from django.test import TestCase
 from newsletter.models import NewsUser
 from datetime import datetime, timedelta, timezone, date
-class TestModel(TestCase):
+class TestNewsletterModel(TestCase):
+    def setUp(self):
+        NewsUser.objects.create(email='email@gmail.com')
+
     #Check if email to be shown in admin is right
-    def test_email_string_representation(self):
-        newUser = NewsUser.objects.create(email='email@gmail.com')
+    def test_newsletter_string_representation(self):
+        newUser = NewsUser.objects.get(pk=1)
         self.assertEqual(str(newUser),newUser.email)
     #Check for blank/null email
-    def test_blank_email(self):
-        newUser = NewsUser.objects.create(email='email@gmail.com')
+    def test_newsletter_blank_null_email(self):
+        newUser = NewsUser.objects.get(pk=1)
         self.assertNotEqual(newUser.email,None)
         self.assertNotEqual(newUser.email,'')
     #Check if datetime is being stored at time of creation
     def test_if_storing_date(self):
-        newUser = NewsUser.objects.create(email='email@gmail.com')
+        newUser = NewsUser.objects.get(pk=1)
         self.assertNotEqual(newUser.date_added,None)
     #check if datetime being stored is right
     def test_if_storing_right_date(self):
-        newUser = NewsUser.objects.create(email='email@gmail.com')
+        newUser = NewsUser.objects.get(pk=1)
         now = datetime.now(timezone.utc)
         self.assertAlmostEqual(newUser.date_added,now,delta=timedelta(seconds=1))
+        
